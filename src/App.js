18de472bbc68ad7f7abee28fb750fcb088e83cde
Rohit Ranjan <os-rranjan@projects.site.sa>
2020-09-06 11:52:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Movie Rator</h1>
+      </header>
+      <div className="layout">
+          <div>Movie List</div>
+          <div>Movie Details</div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
